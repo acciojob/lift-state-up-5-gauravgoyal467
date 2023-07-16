83b8bd9d3a9c1146425127606b81a075591b61df
GAURAV GOYAL <116831689+gauravgoyal467@users.noreply.github.com>
2023-07-16 10:11:21 +0000

    3rd try

--- src/components/Parent.js
+++ src/components/Parent.js
@@ -3,8 +3,6 @@ import Child from './Child';
 
 const Parent = () => {
     const [isLoggedIn,setIsLoggedIn]=useState(false);
-    console.log(isLoggedIn);
-
   return (
     <div>
         <h1>Parent Component</h1>
